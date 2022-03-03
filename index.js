0f5fadd38ba9bdd2c7d3b4dfc9d9c6a6155df30f
tnoonan <tnoonan88@gmail.com>
2022-03-03 16:19:51 -0500

    Wrote inquirer prompt

--- index.js
+++ index.js
@@ -1,7 +1,63 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inqurier');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () =>
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Project name:',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Project description:',
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'Installation instructions:',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Instructions for use:',
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'Contribution guidelines:',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Test instructions:'
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "License information:",
+            choices: 
+            [
+              "The MIT License",
+              "GNU GPL v3",
+              "ISC License (ISC)",
+              "IBM Public License Version 1.0",
+              "Mozilla Public License 2.0",
+            ]
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'GitHub username:',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Email address:'
+        },
+    ]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
