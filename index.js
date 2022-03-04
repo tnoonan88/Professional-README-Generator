49131b7a06d290bf9a29f649761711ecc4adbc67
tnoonan <tnoonan88@gmail.com>
2022-03-04 13:46:18 -0500

    Updated index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
     inquirer.prompt([
         {
             type: 'input',
